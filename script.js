48a90fd81abc67528c5979139a61d77bc6e3f477
AkaKwak <christophe.alaterre@gmail.com>
2024-10-12 13:59:19 +0200

    formulaire 2 en cours + form présence finis + text area dans form 1 + few fixes

--- script.js
+++ script.js
@@ -1,22 +1,55 @@
-document.getElementById('toggleButton').addEventListener('click', function() {
+
+// Gestion du bouton de Choix1 "Présence"
+document.getElementById('toggleButton').addEventListener('click', function () {
+    const formDiv = document.getElementById('presformDiv');
+    const body = document.body; // Référence au body
+    if (formDiv.style.display === 'none' || formDiv.style.display === '') {
+        formDiv.style.display = 'block'; // Afficher la div
+        body.classList.add('dimmed'); // Ajouter la classe pour réduire l'opacité
+
+    } else {
+        formDiv.style.display = 'none'; // Cacher la div
+        body.classList.remove('dimmed'); // Enlever la classe pour rétablir l'opacité
+
+    }
+});
+
+// Gestion du bouton de fermeture "Présence"
+document.getElementById('closeButton').addEventListener('click', function () {
+    const formDiv = document.getElementById('presformDiv');
+    const body = document.body; // Référence au body
+    formDiv.style.display = 'none'; // Cacher la div
+    body.classList.remove('dimmed'); // Enlever la classe pour rétablir l'opacité
+
+});
+
+document.getElementById('toggleButton').addEventListener('click', function () {
     const formDiv = document.getElementById('formDiv');
     const body = document.body; // Référence au body
     if (formDiv.style.display === 'none' || formDiv.style.display === '') {
         formDiv.style.display = 'block'; // Afficher la div
         body.classList.add('dimmed'); // Ajouter la classe pour réduire l'opacité
-        
+
     } else {
         formDiv.style.display = 'none'; // Cacher la div
         body.classList.remove('dimmed'); // Enlever la classe pour rétablir l'opacité
-        
+
     }
 });
 
-// Gestion du bouton de fermeture
-document.getElementById('closeButton').addEventListener('click', function() {
-    const formDiv = document.getElementById('formDiv');
-    const body = document.body; // Référence au body
-    formDiv.style.display = 'none'; // Cacher la div
-    body.classList.remove('dimmed'); // Enlever la classe pour rétablir l'opacité
-   
-});
+
+
+function displayDateTime() {
+    const now = new Date();
+
+    // Formatage de la date et de l'heure
+    const formattedDate = now.toLocaleDateString(); // Format de la date (jj/mm/aaaa)
+    const formattedTime = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); // Format de l'heure (hh:mm)
+
+    // Affichage de la date et de l'heure dans les divs correspondantes
+    document.getElementById('date').textContent = `Date : ${formattedDate}`;
+    document.getElementById('time').textContent = `Heure : ${formattedTime}`;
+}
+
+// Appelle la fonction au chargement de la page
+window.onload = displayDateTime;
